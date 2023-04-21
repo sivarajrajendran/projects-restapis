6b9ea7b1423acf6f796a13e3789038562bcedee7
Sivaraj Rajendran <rsiva108@gmail.com>
2023-04-21 22:28:32 +0530

    Initial files using FastAPI package
    Initial files

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+server_info = {'version': '1.0.0', 'launchDate': '21-04-2023' }
+
+
+@app.get('/info')
+def get_server_info():
+    return {'info': server_info}
